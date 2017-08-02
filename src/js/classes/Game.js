8c420bd6fce5be2b64f20374e1daae7d49fcbf39
Julian <julian.dinges@men.de>
2017-08-02 09:27:14 +0200

    Game is running at proper resolution and pixelated now

--- src/js/classes/Game.js
+++ src/js/classes/Game.js
@@ -5,20 +5,37 @@ class Game {
      * Initialize the game
      */
     constructor() {
-        this.ratio  = window.innerWidth / window.innerHeight;
-        this.width  = window.innerWidth;
-        this.height = window.innerHeight;
-
-        window.addEventListener('resize', () => { this.onResize(this); });
+        this.ratio = 1;
+        this.gameContainer = document.getElementById('g');
+        let rect = this.gameContainer.getBoundingClientRect();
+        this.width  = rect.width;
+        this.height = rect.height;
 
         this.scene = new THREE.Scene();
 
         this.camera = new THREE.PerspectiveCamera(75, this.ratio, 0.1, 20000);
-        this.camera.position.set(0, 0, 75);
+        this.camera.position.z = 400;
 
         this.renderer = new THREE.WebGLRenderer();
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        document.getElementById('g').appendChild(this.renderer.domElement);
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(64, 64);
+        this.gameContainer.appendChild(this.renderer.domElement);
+        this.renderer.domElement.style = null;
+
+        let geometry = new THREE.BoxGeometry(200, 200, 200);
+        let material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+        this.cube    = new THREE.Mesh(geometry, material);
+        this.cube.position.set(0, 0, -50);
+        this.scene.add(this.cube);
+
+        this.light = new THREE.PointLight(0xffffff, 1, 100);
+        this.light.position.set(0, 0, 75);
+        this.scene.add(this.light);
+
+        this.ambient = new THREE.AmbientLight(0x444444, 1);
+        this.scene.add(this.ambient);
+
+        this.runTime = 0.0;
     }
     
     /**
@@ -26,7 +43,7 @@ class Game {
      * @returns {void}
      */
     draw() {
-
+        this.renderer.render(this.scene, this.camera);
     }
 
     /**
@@ -35,21 +52,10 @@ class Game {
      * @returns {void}
      */
     update(dt) {
+        this.runTime += dt;
 
-    }
-
-    /**
-     * Handle browser window resizing
-     */
-    onResize() {
-        this.ratio  = window.innerWidth / window.innerHeight;
-        this.camera.aspect = this.ratio;
-        this.camera.updateProjectionMatrix();
-
-        this.width  = window.innerWidth;
-        this.height = this.width / this.ratio;
-
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.cube.rotation.x += Math.sin(this.runTime) * dt * 0.5;
+        this.cube.rotation.y += Math.cos(this.runTime) * dt;
     }
 }
 
