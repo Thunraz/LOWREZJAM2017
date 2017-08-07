48d5cccd2907efda9ff18d187e175ed5ba34e93c
Julian <julian.dinges@men.de>
2017-08-07 07:45:37 +0200

    Create a source map file

--- tasks/build.js
+++ tasks/build.js
@@ -1,8 +1,9 @@
 'use strict';
 
 let gulp        = require('gulp'),
-    rename      = require('gulp-rename'),
     rollup      = require('rollup').rollup;
+
+let cache;
 
 module.exports = () => {
     gulp.task('copy-dependencies', () => {
@@ -16,11 +17,13 @@ module.exports = () => {
     gulp.task('build', ['copy-dependencies'], (callback) => {
         return rollup({
             entry: 'src/js/main.js',
+            cache: cache,
             sourceMap: true,
             external: ['three']
         }).then(function(bundle) {
             return bundle.write({
                 dest:  'dist/main.js',
+                sourceMap: true,
                 format: 'iife',
                 globals: {
                     three: 'THREE'
