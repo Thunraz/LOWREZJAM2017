ee83b345b489663b017b02000ec30cea15178d98
Julian <julian.dinges@men.de>
2017-08-09 14:30:48 +0200

    Moved camera closer to ship (hard to see at 64 pixels)

--- src/js/GameProperties.js
+++ src/js/GameProperties.js
@@ -1,13 +1,13 @@
 import * as THREE from 'three';
 
 class GameProperties {
-    //*
+    /*
     static get GameSize()            { return 900; }
     /*/
     static get GameSize()            { return 64; }
     //*/
     static get CameraMovementSpeed() { return 200.0; }
-    static get CameraOffset()        { return new THREE.Vector3(0, 400, 200); }
+    static get CameraOffset()        { return new THREE.Vector3(0, 200, 100); }
     static get RenderTextureSize()   { return 512; }
     static get ButtonTimeout()       { return 0.25; }
 
