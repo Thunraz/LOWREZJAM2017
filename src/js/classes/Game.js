a53593d3474265574c0ffebf1f7c53884992dad7
Thunraz <julian.dinges@gmail.com>
2017-08-13 15:50:24 +0200

    Added island with procedurally generated terrain

--- src/js/classes/Game.js
+++ src/js/classes/Game.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 
 import GP           from '../GameProperties.js';
+import Island       from './Island.js';
 import Player       from './Player.js';
 import Port         from './Port.js';
 import WaterSurface from './WaterSurface.js';
@@ -42,6 +43,9 @@ class Game {
 
         this.waterTrail = new WaterTrail(this);
         this.scene.add(this.waterTrail);
+        
+        this.island = new Island(this);
+        this.scene.add(this.island);
 
         this.port = new Port(this);
         this.scene.add(this.port);
