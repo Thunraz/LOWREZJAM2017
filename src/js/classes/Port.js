806c8c57d7bea5234e3140fb96013a62c22eb304
Thunraz <julian.dinges@gmail.com>
2017-08-13 15:49:56 +0200

    Fixed port materials

--- src/js/classes/Port.js
+++ src/js/classes/Port.js
@@ -21,10 +21,37 @@ class Port extends THREE.Object3D {
                 this.port.receiveShadow = true;
                 this.port.scale.set(scale, scale, scale);
                 this.add(this.port);
+                
+                for(let i = 0; i < this.port.children.length; i++) {
+                    if(this.port.children[i].name == 'Houses') {
+                        let houses = this.port.children[i];
+                        for(let j = 0; j < houses.material.length; j++) {
+                            if(houses.material[j].name == 'Walls') {
+                                houses.material[j] = new THREE.MeshStandardMaterial({ color: 0x8c001a });
+                                houses.material[j].name = 'Walls';
+                            } else if(houses.material[j].name == 'Roofs') {
+                                houses.material[j] = new THREE.MeshStandardMaterial({ color: 0x333333 });
+                                houses.material[j].name = 'Roofs';
+                            } else if(houses.material[j].name == 'Hook') {
+                                houses.material[j] = new THREE.MeshStandardMaterial({ color: 0xdddddd });
+                                houses.material[j].name = 'Hook';
+                            } else if(houses.material[j].name == 'Dark') {
+                                houses.material[j] = new THREE.MeshStandardMaterial({ color: 0x0 });
+                                houses.material[j].name = 'Dark';
+                            } else if(houses.material[j].name == 'Ground') {
+                                houses.material[j] = new THREE.MeshStandardMaterial({ color: 0xd86f1c });
+                                houses.material[j].name = 'Ground';
+                            } else if(houses.material[j].name == 'Doors') {
+                                houses.material[j] = new THREE.MeshStandardMaterial({ color: 0x6f7ad5 });
+                                houses.material[j].name = 'Doors';
+                            }
+                        }
+                    }
+                }
             }
         );
 
-        this.rotateY(-Math.PI/2)
+        this.rotateY(-Math.PI / 2);
     }
 }
 
