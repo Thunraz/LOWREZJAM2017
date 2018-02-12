c332b486f9d8b7fd58ba96b03a410982c06e8f66
Julian <julian.dinges@men.de>
2018-02-12 14:30:14 +0100

    Added browser sync

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,6 +1,7 @@
 'use strict';
 
-let gulp       = require('gulp'),
+let browserSync = require('browser-sync').create(),
+    gulp       = require('gulp'),
     http       = require('http'),
     path       = require('path'),
     st         = require('st');
@@ -9,15 +10,27 @@ module.exports = () => {
     gulp.task('watch', ['server'], () => {
         return gulp.watch(
             ['src/js/**/*.js', 'src/sass/**/*.s?ss', 'src/index.pug', 'src/assets/*', 'src/assets/**/*'],
-            ['build', 'css', 'template', 'assets']
+            ['build', 'css', 'template', 'assets', 'reload-browser']
         );
     });
 
-    gulp.task('server', (done) => {
+    gulp.task('server', function() {
+        browserSync.init({
+            server: {
+                baseDir: path.join(__dirname, '..', 'dist')
+            }
+        });
+    });
+
+    gulp.task('reload-browser', function() {
+        browserSync.reload();
+    });
+
+    /*gulp.task('server', (done) => {
         let port = process.env.GULP_PORT || 8080;
         
         http.createServer(
             st({ path: path.join(__dirname, '..', 'dist'), index: 'index.html', cache: false })
         ).listen(port, done);
-    });
+    });*/
 };
