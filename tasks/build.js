3a9b58bc0298db1d0ba83b16965fe6481832def7
Thunraz <thunraz@thunraz.com>
2018-02-19 10:33:56 +0100

    Added linting to build process

--- tasks/build.js
+++ tasks/build.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let gulp        = require('gulp'),
+    eslint      = require('gulp-eslint'),
     rollup      = require('rollup').rollup;
 
 let cache;
@@ -14,16 +15,15 @@ module.exports = () => {
             .pipe(gulp.dest('./dist/'))
     });
 
-    gulp.task('build', ['copy-dependencies'], (callback) => {
+    gulp.task('build', ['lint', 'copy-dependencies'], () => {
         return rollup({
-            entry: 'src/js/main.js',
+            input: 'src/js/main.js',
             cache: cache,
-            sourceMap: true,
             external: ['three']
         }).then(function(bundle) {
             return bundle.write({
-                dest:  'dist/main.js',
-                sourceMap: true,
+                file:  'dist/main.js',
+                sourcemap: true,
                 format: 'iife',
                 globals: {
                     three: 'THREE'
@@ -31,4 +31,21 @@ module.exports = () => {
             });
         });
     });
+
+    gulp.task('lint', () => {
+        // ESLint ignores files with "node_modules" paths.
+        // So, it's best to have gulp ignore the directory as well.
+        // Also, Be sure to return the stream from the task;
+        // Otherwise, the task may end before the stream has finished.
+        return gulp.src(['src/js/**/*.js'])
+            // eslint() attaches the lint output to the "eslint" property
+            // of the file object so it can be used by other modules.
+            .pipe(eslint())
+            // eslint.format() outputs the lint results to the console.
+            // Alternatively use eslint.formatEach() (see Docs).
+            .pipe(eslint.format())
+            // To have the process exit with an error code (1) on
+            // lint error, return the stream and pipe to failAfterError last.
+            .pipe(eslint.failAfterError());
+    })
 };
